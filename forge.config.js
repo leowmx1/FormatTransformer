c40ad561cb9b9e2e3e31c40963df81299f1754b8
王铭瑄 <leowmx@163.com>
2026-02-05 12:54:03 +0800

    暂时禁用squirrel打包

--- forge.config.js
+++ forge.config.js
@@ -11,10 +11,10 @@ module.exports = {
   },
   rebuildConfig: {},
   makers: [
-    {
-      name: '@electron-forge/maker-squirrel',
-      config: {},
-    },
+    // {
+    //   name: '@electron-forge/maker-squirrel',
+    //   config: {},
+    // },
     {
       name: '@electron-forge/maker-zip',
       platforms: ['darwin'],
